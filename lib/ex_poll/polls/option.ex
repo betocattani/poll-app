370537c2201e02be50dab44bdaffa35bcf3e27e3
Cattani <contato.cattani@gmail.com>
2021-06-11 11:18:22 -0300

    add associations

--- lib/ex_poll/polls/option.ex
+++ lib/ex_poll/polls/option.ex
@@ -2,9 +2,11 @@ defmodule ExPoll.Polls.Option do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ExPoll.Polls.Poll
+
   schema "options" do
     field :value, :string
-    field :poll_id, :id
+    belongs_to(:poll, Poll)
 
     timestamps()
   end
@@ -14,5 +16,6 @@ defmodule ExPoll.Polls.Option do
     option
     |> cast(attrs, [:value])
     |> validate_required([:value])
+    |> assoc_constraint(:poll)
   end
 end
--- lib/ex_poll/polls/poll.ex
+++ lib/ex_poll/polls/poll.ex
@@ -2,8 +2,11 @@ defmodule ExPoll.Polls.Poll do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ExPoll.Polls.Option
+
   schema "polls" do
     field :question, :string
+    has_many(:options, Option, on_replace: :delete)
 
     timestamps()
   end
@@ -12,6 +15,7 @@ defmodule ExPoll.Polls.Poll do
   def changeset(poll, attrs) do
     poll
     |> cast(attrs, [:question])
+    |> cast_assoc(:options)
     |> validate_required([:question])
   end
 end
